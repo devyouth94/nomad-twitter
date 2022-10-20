199c30c4fdafd9bd622f003ac414b25f08d74f1c
devyouth94 <devyouth94@gmail.com>
2022-10-20 22:26:57 +0900

    [feat] 리액트 라우터 적용

---
+++ src/routers/Router.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Navigation from "../components/Navigation";
+
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import Auth from "../pages/Auth";
+
+const Router = ({ isAuth }) => {
+  return (
+    <>
+      {isAuth && <Navigation />}
+      <Routes>
+        {isAuth ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Auth />} />
+            <Route path="/profile" element={<Navigate replace to="/" />} />
+          </>
+        )}
+      </Routes>
+    </>
+  );
+};
+
+export default Router;
