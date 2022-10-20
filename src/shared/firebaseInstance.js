c03e7e584c5d25a3b9f4e37d478b7eea97c3d7ae
devyouth94 <devyouth94@gmail.com>
2022-10-20 22:27:35 +0900

    [feat] 파이어 베이스 인스턴스 생성

---
+++ src/shared/firebaseInstance.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIhtECnODBteWPcEIOxJMxXn8gdvit_N8",
+  authDomain: "nomad-twitter-b34b7.firebaseapp.com",
+  projectId: "nomad-twitter-b34b7",
+  storageBucket: "nomad-twitter-b34b7.appspot.com",
+  messagingSenderId: "213646258997",
+  appId: "1:213646258997:web:07cd34ce0e7fc87571527c",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
